b4bddaba9b2c8cdac973f7fec898af444ea732a3
Kodyc1 <Kodyc1@users.noreply.github.com>
2018-03-15 15:16:28 -0700

    still looking for a better format

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 
+// change header to opaque when scrolling past 50
 $(window).scroll(function(){
   if($(window).scrollTop() >= 50){
     $('header').css('background', '#1b1b1b');
     $('h1').css('color','#1affda');
   } else{
-    $('header').css('background', 'rgba(27,27,27,0.7)');
+    $('header').css('background', 'rgba(27,27,27,0.8)');
     $('h1').css('color','white');
   }
 });
